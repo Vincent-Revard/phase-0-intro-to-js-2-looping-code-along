ac07c21e60eca1e64821750de86e63899c5034c9
Vincent Revard <Vincent.Revard@gmail.com>
2024-01-18 14:26:17 -0800

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bowl!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+//
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function writeCards(names, surprise) {
+    let thankYouMessages = [];
+    for (let i =0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`);
+    }
+
+    return thankYouMessages;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+
+function countDown() {
+    let newNumber = [];
+    for (let i = 10; i >= 0; i--) {
+        console.log(i);
+    }
+    return newNumber;
+}
+
+
+
